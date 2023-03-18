5d7e7ffb32787dae276bf86cc176bc295f0372fa
KC Paul <kathclaire.paul@gmail.com>
2023-03-18 07:43:31 -0700

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
 
 function App() {
   return (
-  <div className="App">
-    hello nucamp!
+    <div className="App">
+      <Navbar dark color='primary' sticky='
+    top' expand='md'>
+        <Container>
+          <NavbarBrand>
+            <img src={NucampLogo} alt='nucamp logo'></img>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      hello nucamp!
     </div>
   );
 }
